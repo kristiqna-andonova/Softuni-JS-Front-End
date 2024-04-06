fa8dfe101b2d40c652ed7c7d0d2b4066ff8a8bb4
Kristiqna <123325312+kristiqna-andonova@users.noreply.github.com>
2024-04-06 13:12:07 +0300

    tasks

--- HTTP and AJAX - Exercise/03.Phonebook/app.js
+++ HTTP and AJAX - Exercise/03.Phonebook/app.js
@@ -1,5 +1,57 @@
 function attachEvents() {
-    // TODO:
+    let url = 'http://localhost:3030/jsonstore/phonebook'
+    
+    const listElement = document.getElementById('phonebook')
+
+    const loadButton = document.getElementById('btnLoad')
+    loadButton.addEventListener('click', load)
+
+    const createButton = document.getElementById('btnCreate')
+    createButton.addEventListener('click', onCreate)
+
+    async function load(even){
+        let response = await fetch(url)
+        let date = await response.json()
+
+        for (let phoneObj of Object.entries(date)) {
+            let phoneDate = phoneObj[1]
+
+            let li = document.createElement('li')
+            li.textContent = `${phoneDate.person}: ${phoneDate.phone}`
+
+            let deleteButton = document.createElement('button')
+            deleteButton.textContent = `Delete`
+            deleteButton.addEventListener('click', onDelete)
+
+            li.appendChild(deleteButton)
+            listElement.appendChild(li)
+
+            async function onDelete(){
+                fetch(`${url}/${phoneDate._id}`, {
+                    method: 'DELETE'
+                })
+                li.remove() 
+            }
+        }
+    }
+
+    async function  onCreate(event){
+        let person = document.getElementById('person').value
+        let phone = document.getElementById('phone').value
+
+        let number = {
+            person,phone
+        }
+
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(number)
+        })
+
+        person.value = ''
+        phone.value = ''
+        load()
+    }
 }
 
 attachEvents();
